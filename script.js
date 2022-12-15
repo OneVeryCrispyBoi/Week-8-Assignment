64afd3703da79d40be17464db8718290a92b5f6d
Alexis Lara <111474970+OneVeryCrispyBoi@users.noreply.github.com>
2022-12-14 16:26:17 -0800

    answers

--- script.js
+++ script.js
@@ -4,19 +4,19 @@
 console.log(
   "What worked well for you over the last 9 weeks that aided in your learning process?"
 );
-console.log("");
+console.log("My parents reminding me to stay focused on my work when I am at home.");
 
 // 2. Write a console.log() statement that answers the following question: What would you
 // change about the course?
 
 console.log("What would you change about the course?");
-console.log("");
+console.log("There really isn't anything i would change about the course.");
 
 // 3. Write a console.log() statement that answers the following question: Did you learn
 // what you expected to learn in this course?
 
 console.log("Did you learn what you expected to learn in this course?");
-console.log("");
+console.log("I can't really say I expected anything when I signed up for this course. I've never had any experience in programming before, so everything I learned in this course was new to me.");
 
 // 4. Write a console.log() statement that answers the following question: What concepts
 // were new to you and what concepts had you encountered already in the past?
@@ -24,4 +24,4 @@ console.log("");
 console.log(
   "What concepts were new to you and what concepts had you encountered already in the past?"
 );
-console.log("");
+console.log("Everything to do with Javascript was new to me, because my last course mainly focused on HTML and CSS code.");
